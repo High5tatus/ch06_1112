bfe40397c9096a0ad415e7437b7e5030b9df5b81
High5tatus <wnsdnl125@naver.com>
2021-11-12 13:58:02 +0900

    11-12

--- app/src/main/java/com/example/ch06_1112/MainActivity.java
+++ app/src/main/java/com/example/ch06_1112/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ch06_1112;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -13,22 +15,29 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements ActionBar.TabListener{
 
-    ActionBar.Tab tabSong, tabArtist, tabAlbum;
-
-    MyTabFragment myFrags[] = new MyTabFragment[3];
+    ActionBar.Tab tabSong, tabArtist, tabAlbum;//프래그먼트랑 연결해야하기 때문에 멤버변수로 격상되었음
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        //예제 6-19
+        //1."액션바" 만들기
+        //2."탭" 만들기 - 액션바에 들어갈 탭
+        //3."프레그먼트" 만들기
+        //4. 탭과 프래그먼트 연결하기
+
+        //1."액션바" 만들기
         ActionBar bar = getSupportActionBar();
         bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        tabSong = bar.newTab();
-        tabSong.setText("음악별");
-        tabSong.setTabListener(this);
-        bar.addTab(tabSong);
+        //2."탭" 만들기 - 액션바에 들어갈 탭
+//        ActionBar.Tab tabSong, tabArtist, tabAlbum;//프래그먼트랑 연결해야하기 때문에 멤버변수로 격상
+
+        tabSong = bar.newTab();//탭 만들기
+        tabSong.setText("음악별");//탭 옵션정하기
+//        tabSong.setTabListener(리스너) 탭에 리스너를 달기(이벤트처리)
+        tabSong.setTabListener(this);//(리스너) 탭에 리스너를 달기(이벤트처리)
+        bar.addTab(tabSong);//bar에 탭을 추가
 
         tabArtist = bar.newTab();
         tabArtist.setText("가수별");
@@ -44,18 +53,39 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-        MyTabFragment myTabFrag = null;
 
-        if(myFrags[tab.getPosition()] == null){
+        MyTabFragment myTabFrag = null;//맴버변수로 격상
+        MyTabFragment[] myFrags = new MyTabFragment[3];
+
+        if(myFrags[tab.getPosition()] == null){ //눌러진 탭인텍스가 배열에 있나 없나 조회
+            //처음이라면
+            //1.프래그먼트 생성 -> new MyFragment()
+            //2.옵션: 프래그먼트 안에 리니어 색상 결정 -> Os로부터 받아온 Bundle의 키 중에서 "tabName"
+            //3.프래그먼트와 탭연결
+            //4.새로만든 프래그먼트1개 짜리를 프래그먼트배열에 넣어주기
+
+            //1.프래그먼트 생성 -> new MyFragment()
             myTabFrag = new MyTabFragment();
+
+            //2.옵션: 프래그먼트 안에 리니어 색상 결정
+            // -> OS로 보내려면 Bundle이 필요함
+            //Bundle안에 key를 tabName으로 정함
+            //tabName 안에 "가수별","앨범별","음악별" 이런 실제값을 보냄,
             Bundle data = new Bundle();
             data.putString("tabName", tab.getText().toString());
             myTabFrag.setArguments(data);
+
+            //3.프래그먼트와 탭연결// 저아래서 공용으로
+//            ft.replace(android.R.id.content, myTabFrag);
+
+            //4.새로만든 프래그먼트1개 짜리를 프래그먼트배열에 넣어주기
+            //나중을 위해서, 배열안에 프래그 있는지로 처음인지 아닌지 판별
             myFrags[tab.getPosition()] = myTabFrag;
         }
-        else
-            myTabFrag = myFrags[tab.getPosition()];
+        else//처음아님, 아까 왔었음
+            myTabFrag = myFrags[tab.getPosition()];//프래그먼트 생성 안함! 왜? 배열안에 있으니까, 그거 꺼내옴
 
+        //3.프래그먼트와 탭연결
         ft.replace(android.R.id.content, myTabFrag);
     }
 
@@ -69,16 +99,29 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 
     }
 
-
+    //3."프레그먼트" 만들기
+    //멤버 자리에 - 1.멤변, 2.생성자, 3.메소드, 4,내부클래스
     public static class MyTabFragment extends androidx.fragment.app.Fragment{
 
-        String tabName;
+        //alt + insert 키로, override method
+        //1.onCreate() 와 2.onCreateView() 메소드 2개를 재정의한다.
+
+        String tabName;//멤버변수로 격상됨, 왜? onCreateView()에 쓸꺼니까
+
+        //1.탭과 프래그먼트(=me)를 연결하기 위해 탭이름과 연동 설정
+        @Override
         public void onCreate(Bundle saveInstanceState) {
             super.onCreate(saveInstanceState);
-            Bundle data = getArguments();
-            tabName = data.getString("tabName");
+
+            Bundle data = getArguments();//OS와 통신
+            tabName = data.getString("tabName");//OS의 Bundle의 키로 값을 가져옴
         }
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        //프레그먼트(내부에)가 가지는 들어갈 디자인 만들기, 예)리니어레이아웃
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater,@NonNull ViewGroup container,@NonNull Bundle savedInstanceState){
+            //프레그먼트 뷰임, 리니어레이아웃을 하나 가질 예정임
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.MATCH_PARENT);
@@ -91,6 +134,8 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
             if(tabName=="앨범별") baseLayout.setBackgroundColor(Color.BLUE);
 
             return baseLayout;
+//            return 리니어;
+//            return super.onCreateView(inflater, container, savedInstanceState);
         }
     }
 
